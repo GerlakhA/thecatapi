e190f51ef5e4aa73991a4ad202d2ddebfe612138
GerlakhA <andrey215gerlakh@gmail.com>
2026-04-07 16:02:34 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src')
+
+export default defineConfig(({ mode }) => {
+	const isProd = mode === 'production'
+
+	return {
+		base: isProd ? '/thecatapi/' : '/',
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, './src')
+			}
 		}
 	}
 })
